b58bce3a63bd042de1db5a35bc0dfe00a0043b94
sunil srivatsav samsani <sunilsrivatsav9@gmail.com>
2021-10-21 21:48:06 +0900

    global variable

--- sorting/binary_search.py
+++ sorting/binary_search.py
@@ -1,7 +1,8 @@
 list = [1,6,9,15,34,55,78]
+## list should be sorted for binary search
 list.sort()
 
-pos=-1
+position = -1
 def binarysearch(n,list):
     L = 0
     U = len(list)-1
@@ -9,7 +10,7 @@ def binarysearch(n,list):
         #### calculate mid and compare the value
         mid = (L+U)//2
         if list[mid]==n:
-            globals()['pos']=mid
+            globals()['position']=mid
             return True
         else:
             ####  move the lower bound right
@@ -22,6 +23,6 @@ def binarysearch(n,list):
 
 n = 46
 if binarysearch(n,list):
-    print("found at: ", pos)
+    print("found at: ", position)
 else:
     print("not found")
