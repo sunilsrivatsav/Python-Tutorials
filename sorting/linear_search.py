c97dd8e9575fa632807dc5aab32479459938415b
sunil srivatsav samsani <sunilsrivatsav9@gmail.com>
2021-10-21 21:28:12 +0900

    Comments

---
+++ sorting/linear_search.py
@@ -0,0 +1,16 @@
+list = [23,8,1,5,9,1,18,65]
+
+pos = -1
+def linearsearch(n):
+
+    for i,num in enumerate(list):
+        if num==n:
+            globals()['pos'] = i
+            return True
+    return False
+
+n = 65
+if linearsearch(n):
+    print("found at : ", pos)
+else:
+    print("not found")
