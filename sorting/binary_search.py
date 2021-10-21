ffe05c5b89505933991fee1af73cda2ca934242a
Sunil Srivatsav Samsani <62248984+sunilsrivatsav@users.noreply.github.com>
2021-10-21 22:10:13 +0900

    Add files via upload

---
+++ sorting/binary_search.py
@@ -0,0 +1,27 @@
+list = [1,6,9,15,34,55,78]
+list.sort()
+
+pos=-1
+def binarysearch(n,list):
+    Lower = 0
+    Upper = len(list)-1
+    while(Lower<=Upper):
+        #### calculate mid and compare the value
+        mid = (Lower+Upper)//2
+        if list[mid]==n:
+            globals()['pos']=mid
+            return True
+        else:
+            ####  move the lower bound right
+            if list[mid]<n:
+                Lower = mid+1
+            ##### move the upper bound left
+            else:
+                Upper=mid-1
+    return False
+
+n = 46
+if binarysearch(n,list):
+    print("found at: ", pos)
+else:
+    print("not found")
---
+++ sorting/bubble_sort.py
@@ -0,0 +1,18 @@
+
+list = [23,8,1,5,9,1,18,65]
+
+
+def bubblesort(list):
+    ### outer loop goes from n, n-1, n-2, ....
+    for i in range(len(list)-1,0,-1):
+        ####Inner loop for comparing two numbers
+        #### finds max number and pushes to right corner
+        for j in range(i):
+            if list[j]>list[j+1]:
+                temp = list[j]
+                list[j] = list[j+1]
+                list[j+1] = temp
+    print(list)
+
+
+bubblesort(list)
